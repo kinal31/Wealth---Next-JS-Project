8f913bb148a29d46a550ee4ffaa0f4c6340e8723
Kinal <128071723+kinal31@users.noreply.github.com>
2024-12-28 11:35:01 +0530

    28/12 -second

--- app/layout.js
+++ app/layout.js
@@ -15,6 +15,9 @@ export default function RootLayout({ children }) {
   return (
     <ClerkProvider>
       <html lang="en">
+        <head>
+          <link rel="icon" href="/logo-sm.png" sizes="any" />
+        </head>
         <body className={`${inter.className}`}>
           {/* header */}
           <Header />
